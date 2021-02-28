92d313c5c5738214211f27936550f578ca73e32a
End User <End User@DESKTOP-62AKJQO>
2021-02-28 17:25:13 -0600

    Link Git

---
+++ src/primerproyectojava_matrícula66228/PrimerProyectoJava_matrícula66228.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_matrícula66228;
+
+/**
+ *
+ * @author End User
+ */
+public class PrimerProyectoJava_matrícula66228 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        int avion = 3;
+        System.out.print("\n " + avion + "\n ");
+        
+        char enye = 'ñ';
+        System.out.print(enye + "\n ");
+        
+        boolean comer = true;
+        System.out.print(comer + "\n ");
+        
+    }
+    
+}
